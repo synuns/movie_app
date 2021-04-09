bd33cbc5188354672cd17363e157fb5d3a59647a
Jdentity <identityjang@gmail.com>
2021-04-09 23:37:33 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,67 +1,47 @@
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
+import React from "react";
 
-const foodILike = [
-  {
-      id : 1,
-      name : "Chicken",
-      image : "https://images.unsplash.com/photo-1585039261108-ec658b915e37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=728&q=80",
-      rating : 5.0
-  },
-  {
-      id : 2,
-      name : "Pizza",
-      image : "https://images.unsplash.com/photo-1574126154517-d1e0d89ef734?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
-      rating : 4.6
-  },
-  {
-      id : 3,
-      name : "Hamburger",
-      image : "https://images.unsplash.com/photo-1561758033-d89a9ad46330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-      rating : 4.8
-  },
-  {
-      id : 4,
-      name : "spaghetti",
-      image : "https://images.unsplash.com/photo-1608334481162-bba440193a20?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-      rating : 4.5
-  },
-  {
-      id : 5,
-      name : "yogurt",
-      image : "https://images.unsplash.com/reserve/YFdIoUsRJCAehcoUnQaS_Straw.jpg?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1047&q=80",
-      rating : 4.8
-  },
-];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
 
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  )
-}
+  state = {
+    count: 0
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
+  add = () => {
+    this.setState(current => ({ count: current.count+1 }));
+  };
 
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(dish => (
-        <Food 
-          key={dish.id} 
-          name={dish.name} 
-          picture={dish.image} 
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  minus = () => {
+    this.setState(current => ({ count: current.count-1 }));
+  };
+  
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("component updated");
+  }
+
+  componentWillUnmount() {
+    console.log("component unmounted");
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>I'm a class</h1>
+        <h2>count:{this.state.count}</h2>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
