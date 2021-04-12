28524ab0bd5e570f7da5c18658ee68819b8bf63d
Jdentity <identityjang@gmail.com>
2021-04-12 13:17:05 +0900

    start movie app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-//import PropTypes from "prop-types";
 import React from "react";
 import axios from "axios";
+import "./App.css";
 import Movie from "./Movie";
-import "./App.css"
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
-  };
+  }
 
   getMovies = async () => {
     const {
         data: {
-            data: { movies }
+          data: {movies}
         }
     } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
     this.setState({ movies, isLoading: false });
@@ -22,21 +21,30 @@ class App extends React.Component {
   componentDidMount() {
     this.getMovies();
   }
-
+  
   render() {
     const {isLoading, movies} = this.state;
-    return (<section class="container">
-        { isLoading ? (
+    return (
+      <section class="container">
+        {isLoading ? (
           <div class="loader">
             <span class="loader__text">Loading...</span>
           </div>
-        ) : (
-          movies.map(movie => (
-            <Movie key={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-          ))
+        ):(
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} />
+            ))}
+          </div>
         )}
-    </section>);
-    }
+      </section>
+    );
   }
+}
 
 export default App;
